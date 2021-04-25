ddf0e4521482c971e2fe48f4247c9f2b2c051034
David Baron <info@nerdtron.com>
2021-04-24 22:04:07 -0400

    Refactoring the InventorySystem and it's adapter

--- Assets/Chapters/Adapting Systems with an Adapter/Scripts/InventorySystem.cs
+++ Assets/Chapters/Adapting Systems with an Adapter/Scripts/InventorySystem.cs
@@ -1,11 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Chapter.Adapter
 {
     public class InventorySystem
     {
-        // ....
-
         public void AddItem(int itemID)
         {
             Debug.Log("Adding item to the cloud");
@@ -16,6 +15,10 @@ namespace Chapter.Adapter
             Debug.Log("Removing item from the cloud");
         }
 
-        // ....
+        public List<int> GetInventoryList()
+        {
+            Debug.Log("Returning item list stored in the cloud");
+            return new List<int>(); // returning empty list for testing purposes
+        }
     }
 }
--- Assets/Chapters/Adapting Systems with an Adapter/Scripts/InventorySystemAdapter.cs
+++ Assets/Chapters/Adapting Systems with an Adapter/Scripts/InventorySystemAdapter.cs
@@ -4,8 +4,6 @@ namespace Chapter.Adapter
 {
     public class InventorySystemAdapter
     {
-        public bool saveToDisk;
-
         private readonly InventorySystem _inventorySystem;
         
         public InventorySystemAdapter(InventorySystem inventorySystem)
@@ -17,27 +15,27 @@ namespace Chapter.Adapter
         {
             _inventorySystem.AddItem(itemID);
 
-            if (saveToDisk)
-            {
-                // Implement here logic that calls local save system
-                Debug.Log("Adding item {number} to local save".Replace("{number}", itemID.ToString()));
-            }
+            // TODO: Add item to local save file
+            
+            Debug.Log("Adding item to local save file");
         }
 
         public void RemoveItem(InventorySystem inventorySystem, int itemID)
         {
             _inventorySystem.AddItem(itemID);
-
-            if (saveToDisk)
-            {
-                // Implement here logic that calls local save system
-                Debug.Log("Removing item {number} from local save".Replace("{number}", itemID.ToString()));
-            }
+            
+            // TODO: Remove item from local save file
+            
+            Debug.Log("Removing item from local save file");
         }
 
-        public void SyncToCloud()
+        public void SyncInventory()
         {
-            // This method syncs the local and cloud inventory
+            var list = _inventorySystem.GetInventoryList();
+            
+            // TODO: Merge the cloud and local inventory
+            
+            Debug.Log("Synchronizing  inventories");
         }
     }
 }
