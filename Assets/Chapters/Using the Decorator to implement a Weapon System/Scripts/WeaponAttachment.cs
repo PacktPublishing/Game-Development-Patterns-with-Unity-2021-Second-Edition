4968beda655f0a6542d464290b354df08874f669
David Baron <david@nerdtron.com>
2021-05-15 21:36:01 -0400

    Refactoring Decorator ScriptableObjects

--- Assets/Chapters/Using the Decorator to implement a Weapon System/Scripts/WeaponAttachment.cs
+++ Assets/Chapters/Using the Decorator to implement a Weapon System/Scripts/WeaponAttachment.cs
@@ -5,10 +5,20 @@ namespace Chapter.Decorator
     [CreateAssetMenu(fileName = "NewAttachment", menuName = "Weapon/Attachment", order = 2)]
     public class WeaponAttachment : ScriptableObject
     {
+        [Range(0, 50)]
         public float range;
-        public float duration;
+        
+        [Range(0, 100)]
         public float strength;
+        
+        [Range(0, 3)]
+        [Tooltip("Extend the firing duration")]
+        public float duration;
+        
+        [Range(0, -10f)]
+        [Tooltip("Reduce the time in takes to cooldown")]
         public float cooldown;
+        
         public string attachmentName;
         public GameObject attachmentPrefab;
         public string attachmentDescription;
--- Assets/Chapters/Using the Decorator to implement a Weapon System/Scripts/WeaponConfig.cs
+++ Assets/Chapters/Using the Decorator to implement a Weapon System/Scripts/WeaponConfig.cs
@@ -5,10 +5,20 @@ namespace Chapter.Decorator
     [CreateAssetMenu(fileName = "NewConfig", menuName = "Weapon/Config", order = 1)]
     public class WeaponConfig : ScriptableObject
     {
+        [Range(0, 50)]
         public float range;
-        public float duration;
+        
+        [Range(0, 100)]
         public float strength;
+        
+        [Range(0, 3)]
+        [Tooltip("Duration of the laser beam in seconds")]
+        public float duration;
+        
+        [Range(0, 10f)]
+        [Tooltip("The amount of seconds to cooldown the weapon")]
         public float cooldown;
+        
         public string weaponName;
         public GameObject weaponPrefab;
         public string weaponDescription;
