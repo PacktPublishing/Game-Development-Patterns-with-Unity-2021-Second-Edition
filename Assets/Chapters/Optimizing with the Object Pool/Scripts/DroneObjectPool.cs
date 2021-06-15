59f9bb112fbb43a59c7fa34123d97efeda039ac7
David Baron <david@nerdtron.com>
2021-06-14 23:43:10 -0400

    Refactoring Object Pool code example

--- Assets/Chapters/Optimizing with the Object Pool/Scripts/DroneObjectPool.cs
+++ Assets/Chapters/Optimizing with the Object Pool/Scripts/DroneObjectPool.cs
@@ -1,28 +1,37 @@
 using UnityEngine;
 using UnityEngine.Pool;
 
-namespace Chapter.ObjectPool
+namespace Chapter.ObjectPool 
 {
     public class DroneObjectPool : MonoBehaviour
     {
         public int maxPoolSize = 10;
 
-        public IObjectPool<Drone> Pool
+        public IObjectPool<Drone> Pool 
         {
-            get
+            get 
             {
                 if (_pool == null)
                     _pool = 
-                        new ObjectPool<Drone>(CreatedPooledItem, OnTakeFromPool, OnReturnedToPool, OnDestroyPoolObject, true, 5, maxPoolSize);
+                        new ObjectPool<Drone>(
+                            CreatedPooledItem, 
+                            OnTakeFromPool, 
+                            OnReturnedToPool, 
+                            OnDestroyPoolObject, 
+                            true, 
+                            5, 
+                            maxPoolSize);
                 return _pool;
             }
         }
 
         private IObjectPool<Drone> _pool;
 
-        private Drone CreatedPooledItem()
+        private Drone CreatedPooledItem() 
         {
-            var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var go = 
+                GameObject.CreatePrimitive(PrimitiveType.Cube);
+            
             Drone drone = go.AddComponent<Drone>();
             
             go.name = "Drone";
@@ -31,26 +40,26 @@ namespace Chapter.ObjectPool
             return drone;
         }
 
-        private void OnReturnedToPool(Drone system)
+        private void OnReturnedToPool(Drone system) 
         {
             system.gameObject.SetActive(false);
         }
 
-        private void OnTakeFromPool(Drone system)
+        private void OnTakeFromPool(Drone system) 
         {
             system.gameObject.SetActive(true);
         }
 
-        private void OnDestroyPoolObject(Drone system)
+        private void OnDestroyPoolObject(Drone system) 
         {
             Destroy(system.gameObject);
         }
 
-        public void Spawn()
+        public void Spawn() 
         {
             var amount = Random.Range(1, 10);
-            for (int i = 0; i < amount; ++i)
-            {
+            
+            for (int i = 0; i < amount; ++i) {
                 var drone = Pool.Get();
                 
                 drone.transform.position = 
