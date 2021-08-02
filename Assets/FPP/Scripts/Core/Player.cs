76a343a0e428dc70392e5f8353ffeef751f4caa8
David Baron <david@nerdtron.com>
2021-08-02 10:28:03 -0400

    [FPP] - Integrating changes to GameManager class for FPP project

--- Assets/FPP/Scripts/Core/Player.cs
+++ Assets/FPP/Scripts/Core/Player.cs
@@ -6,4 +6,5 @@ public class Player
     public int currentTrack;
     public string playerName;
     public string playerUid; // TODO: Implement a UID generator
+    public TimeSpan lastSessionDuration;
 }
--- Assets/FPP/Scripts/Managers/GameManager.cs
+++ Assets/FPP/Scripts/Managers/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using FPP.Scripts.Systems;
+﻿using System;
+using FPP.Scripts.Systems;
 using FPP.Scripts.Patterns;
 using UnityEngine.SceneManagement;
 
@@ -6,6 +7,12 @@ namespace FPP.Scripts.Managers
 {
     public class GameManager : Singleton<GameManager>
     {
+        private Player _player;
+        private SaveSystem _saveSystem;
+        
+        private DateTime _sessionStartTime;
+        private DateTime _sessionEndTime;
+
         void Start()
         {
             InitGame();
@@ -13,13 +20,32 @@ namespace FPP.Scripts.Managers
 
         private void InitGame()
         {
-            SaveSystem saveSystem = new SaveSystem();
-            Player player = saveSystem.LoadPlayer();
-
-            if (player == null)
+            _sessionStartTime = DateTime.Now;
+            
+            _saveSystem = new SaveSystem();
+            _player = _saveSystem.LoadPlayer();
+            
+            if (_player == null)
                 SceneManager.LoadScene("Registration");
             else
                 SceneManager.LoadScene("MainMenu");
+        }
+
+        private void GetDailyChallenges()
+        {
+            // TODO: Implement call to endpoint to get daily challenges from the backend
+        }
+        
+        void OnApplicationQuit() 
+        {
+            _sessionEndTime = DateTime.Now;
+            
+            TimeSpan timeDifference = 
+                _sessionEndTime.Subtract(_sessionStartTime);
+
+            _player.lastSessionDuration = timeDifference;
+            
+            _saveSystem.SavePlayer(_player);
         }
     }
 }
