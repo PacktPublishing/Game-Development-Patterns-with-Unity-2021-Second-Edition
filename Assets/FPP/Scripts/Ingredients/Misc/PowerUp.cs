30825cd1fc1383e066d31d5f21216bfaf0886af2
David Baron <david@nerdtron.com>
2021-08-08 21:57:00 -0400

    [FPP] - Integrating general refactoring of the FPP project

---
+++ Assets/FPP/Scripts/Ingredients/Misc/PowerUp.cs
@@ -0,0 +1,40 @@
+using TMPro;
+using UnityEngine;
+using FPP.Scripts.Controllers;
+
+namespace FPP.Scripts.Ingredients.Misc
+{
+    public class PowerUp : MonoBehaviour
+    {
+        public PowerUpItem powerup;
+        private TextMeshPro _title;
+
+        private void Start()
+        {
+            if (powerup)
+            {
+                _title = GetComponentInChildren<TextMeshPro>();
+                _title.text = powerup.title;
+
+                // TODO: Write a custom shader for the powerups
+                //var render = GetComponent<Renderer>();
+                //render.material.SetColor("_Color", powerup.pickupColor);
+            }
+            else
+            {
+                Debug.LogError("Missing powerup item!");
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            BikeController controller = other.GetComponent<BikeController>();
+            
+            if (controller)
+            {
+                controller.Accept(powerup);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
